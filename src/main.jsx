8ce874e282ba0580b7fbb7234b05ad607e3610e5
Mega Destro <hks01110@gmail.com>
2026-01-11 14:05:35 +0530

    Add debug logging

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import ErrorBoundary from './ErrorBoundary.jsx'
 import './index.css'
 
+console.log("Starting The Converter App...");
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <ErrorBoundary>
